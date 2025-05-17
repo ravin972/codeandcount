fe57d5c76b1fe453832579f2b3accf6f22bf75d0
Ravinder Pandey <pandeyravinder078@gmail.com>
2025-05-17 06:17:05 +0000

    try this : 1. #000007 2. #b5a1a9 3. #d89d10

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export const metadata: Metadata = {
     images: ['https://placehold.co/1200x630.png?text=CodeAndCount'], 
   },
   manifest: '/manifest.json',
-  themeColor: '#0cbf50', // Updated to new accent color
+  themeColor: '#d89d10', // Updated to new accent color Dark Gold
 };
 
 export default function RootLayout({
