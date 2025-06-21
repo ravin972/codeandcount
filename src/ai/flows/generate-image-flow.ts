d1ae17fcc0a6bd5624fd6762d69b2a4a1b578aef
Ravinder Pandey <pandeyravinder078@gmail.com>
2025-06-21 15:06:37 +0000

    i make live this website using vercel , but now : the issue is rising wi

--- src/ai/flows/generate-image-flow.ts
+++ src/ai/flows/generate-image-flow.ts
@@ -37,7 +37,7 @@ const generateImageFlow = ai.defineFlow(
   },
   async (input: GenerateImageInput) => {
     const {media} = await ai.generate({
-      model: 'googleai/gemini-2.0-flash-exp', // Must use this model for image generation
+      model: 'googleai/gemini-2.0-flash-preview-image-generation', // Must use this model for image generation
       prompt: input.prompt,
       config: {
         responseModalities: ['TEXT', 'IMAGE'], // Must provide both TEXT and IMAGE
