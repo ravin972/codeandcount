84e33b50f8184ff650b9dc11db0a08edb75db31b
Ravinder Pandey <pandeyravinder078@gmail.com>
2025-08-14 15:32:07 +0000

    still getting error: and you can use this also: EMAIL_BACKEND = 'django.

--- src/components/forms/ContactForm.tsx
+++ src/components/forms/ContactForm.tsx
@@ -68,7 +68,7 @@ export function ContactForm() {
     setIsSubmitting(true);
     
     try {
-      const response = await fetch("https://formspree.io/f/mvojozpb", {
+      const response = await fetch("https://formspree.io/f/xgegvdpl", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
